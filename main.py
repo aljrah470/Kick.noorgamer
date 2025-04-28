6a6913153a7f9f14e9c4072bd2496565bd3203d4
aljrah470 <aljrahalmntsr10@gmail.com>
2025-04-28 21:45:29 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import datetime
 from selenium import webdriver  
 from selenium.webdriver.common.by import By  
 from selenium.webdriver.common.keys import Keys  
+from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager  
   
 # بيانات الحساب  
@@ -22,7 +23,8 @@ def start_bot():
             options.add_argument("--no-sandbox")  
             options.add_argument("--disable-dev-shm-usage")  
   
-            driver = webdriver.Chrome(options=options)  
+            service = Service(ChromeDriverManager().install())
+            driver = webdriver.Chrome(service=service, options=options)
   
             driver.get("https://kick.com/login")  
             time.sleep(5)  
